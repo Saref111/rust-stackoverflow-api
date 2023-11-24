a67cea8c90d66af4a6e227ffa1a3bab8ada34c81
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-24 08:24:23 +0200

    added routes

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -25,17 +25,23 @@ pub async fn delete_question(
 
 // ---- CRUD for Answers ----
 
-// TODO: Create a POST route to /answer which accepts an `Answer` and returns `AnswerDetail` as JSON.
-//       The handler function should be called `create_answer`.
-//       
-//       hint: this function should look very similar to the create_question function above
+#[post("/answer", data = "<answer>")]
+pub fn create_answer(
+    answer: Json<Answer>,
+) -> Json<Vec<AnswerDetail>> {
+    todo!()
+}
 
-// TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
-//       The handler function should be called `read_answers`.
-//       
-//       hint: this function should look very similar to the read_questions function above
+#[get("/answers", data = "<question_uuid>")]
+pub fn read_answers(
+    question_uuid: Json<QuestionId>
+) -> Json<Vec<AnswerDetail>> {
+    todo!()
+}
 
-// TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
-//       The handler function should be called `delete_answer`.
-//       
-//       hint: this function should look very similar to the delete_question function above
+#[delete("/answer", data = "<answer_uuid>")]
+pub fn delete_answer(
+    answer_uuid: Json<AnswerId>
+) {
+    todo!()
+}
