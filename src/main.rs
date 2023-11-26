d1b040f1d1814b040030ff556b78613a804261f2
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-26 11:07:18 +0200

    added db connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 #[macro_use]
 extern crate rocket;
+use std::env;
+use std::error::Error;
+use log::{info, error};
+use pretty_env_logger;
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
 
 mod cors;
 mod handlers;
@@ -10,6 +16,30 @@ use handlers::*;
 
 #[launch]
 async fn rocket() -> _ {
+    // TODO: Initialize pretty_env_logger
+    pretty_env_logger::init();
+    // TODO: Initialize dotenv
+    dotenv().ok().expect("Failed to read .env file");
+
+    // Create a new PgPoolOptions instance with a maximum of 5 connections.
+    // Use dotenv to get the database url. 
+    // Use the `unwrap` or `expect` method instead of handling errors. If an
+    // error occurs at this stage the server should be terminated. 
+    // See examples on GitHub page: https://github.com/launchbadge/sqlx
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(env::var("DATABASE_URL").unwrap().as_str()).await.unwrap();
+
+    // Using slqx, execute a SQL query that selects all questions from the questions table.
+    // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
+    // make sure we can connect to the database.  
+    let recs = sqlx::query!("SELECT * FROM questions")
+        .fetch_all(&pool).await.unwrap();
+
+    info!("********* Question Records *********");
+    // TODO: Log recs with debug formatting using the info! macro
+    info!("{:?}", recs);
+
     rocket::build()
         .mount(
             "/",
